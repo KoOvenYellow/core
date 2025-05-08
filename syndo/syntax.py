5f5f176e973e7a44d713042958fb8bab47b6d157
MACLA <makuragikanna@gmail.com>
2025-05-08 23:01:59 +0900

    Add syntax_vector placeholder
    First function of the KoOvenYellow syntax engine.
    Converts a sentence into a dummy syntax vector.

---
+++ syndo/syntax.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def syntax_vector(sentence: str) -> np.ndarray:
+    """
+    Generate a dummy syntax vector from a sentence.
+    This is a placeholder for future KoOvenYellow logic.
+    """
+    return np.array([ord(c) for c in sentence])[:10]
